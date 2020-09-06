d82c613b364ba436f53473d1ba7f7a4000c7925e
道化師 <wraith13@hotmail.co.jp>
2020-09-07 08:03:30 +0900

    🛠 menu

---
+++ source/menu.ts
@@ -0,0 +1,79 @@
+import * as vscode from 'vscode';
+export interface CommandMenuItem extends vscode.QuickPickItem
+{
+    when?: (menus: CommandMenuItem[]) => boolean;
+    preview?: () => Promise<unknown>;
+    command?: () => Promise<unknown>;
+    tags?: string[];
+}
+export interface QuickPickOptions extends vscode.QuickPickOptions
+{
+    rollback?: () => Promise<unknown>;
+    strictRollback?: () => Promise<unknown>;
+    debug?: boolean;
+}
+export const showQuickPick = async <T extends CommandMenuItem>
+(
+    items: T[] | Thenable<T[]>,
+    options?: QuickPickOptions,
+    token?: vscode.CancellationToken
+) =>
+{
+    let lastPreview = options ?.strictRollback ?? options ?.rollback;
+    const apply = async (method: (() => Promise<unknown>) | undefined) =>
+    {
+        if (method && lastPreview !== method)
+        {
+            lastPreview = method;
+            if (options?.debug)
+            {
+                await method();
+            }
+            else
+            {
+                try
+                {
+                    await method();
+                }
+                catch
+                {
+                    //  適用のキャンセルにより、大量の `rejected promise not handled` でログを汚すことになってしまうので握り潰す。
+                    //  握り潰す代わりになにかログを吐いてしまうと結局同じ事だし・・・
+                    //  尚、ここでエラーになったからと言って、 return false しちゃうのはロジック的にダメ
+                }
+            }
+            return true;
+        }
+        return false;
+    };
+    const solidItems = await items;
+    const result = await vscode.window.showQuickPick
+    (
+        solidItems
+            .filter(i => undefined === i.when || i.when(solidItems)),
+        Object.assign
+        (
+            {
+                onDidSelectItem: async (item: T) =>
+                {
+                    await apply(options?.strictRollback);
+                    // tslint:disable-next-line: no-unused-expression
+                    await apply(item?.preview) || await apply(options?.rollback);
+                }
+            },
+            options ?? { },
+        ),
+        token
+    );
+    await apply(options?.strictRollback);
+    if (result)
+    {
+        // tslint:disable-next-line: no-unused-expression
+        await apply(result.command) || await apply(result.preview);
+    }
+    else
+    {
+        await apply(options ?.rollback);
+    }
+    return result;
+};
